9ff9b1a02c74c0e67885ce9b30a634cce653cb84
Phantomlsh <phantomlsh@outlook.com>
2022-02-06 10:01:43 -0800

    Add firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC7WZFIjRUQ01A-SCVs0V_bNdhC8YCPzoI',
@@ -16,3 +17,5 @@ const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
 
 export default app
+
+export const db = getFirestore()
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.js'
 
 async function login () {
   const provider = new GoogleAuthProvider(), auth = getAuth()
@@ -18,9 +20,15 @@ async function login () {
   }
 }
 
+async function data () {
+  const docRef = doc(db, 'cache', 'waitz')
+  const docSnap = await getDoc(docRef)
+  if (docSnap.exists()) console.log(docSnap.data())
+}
 </script>
 
 <template>
   <h1>GoGaucho</h1>
   <button @click="login">Try to login</button>
+  <button @click="data">Try to get data</button>
 </template>
