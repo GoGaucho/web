51f9bc06b27a650050fa9d81d9bdf7bd0cf4c87f
Phantomlsh <phantomlsh@outlook.com>
2022-02-06 14:50:28 -0800

    Add Signout

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,13 +1,15 @@
 <script setup>
+import User from './User.vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 import { UserCircleIcon } from '@heroicons/vue/solid'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
-let avatar = $ref('')
+let userInfo = $ref(null), showUser = $ref(false)
 const auth = getAuth()
 auth.onAuthStateChanged(user => {
-  avatar = user?.photoURL
+  userInfo = user
+  if (!user) return showUser = false
 })
 
 function login () {
@@ -24,7 +26,8 @@ function login () {
     <h1 class="text-2xl font-bold flex items-center cursor-pointer group" @click="router.push('/')">
       <img class="mr-2 w-8 rounded" src="/logo.png">GoGaucho
     </h1>
-    <img v-if="avatar" class="w-8 rounded-full" :src="avatar">
+    <img v-if="userInfo" class="w-8 rounded-full cursor-pointer" :src="userInfo.photoURL" @click="showUser = true">
     <user-circle-icon v-else @click="login" class="w-8 cursor-pointer text-gray-500" />
   </div>
+  <user v-model="showUser" :user="userInfo" />
 </template>
---
+++ src/components/User.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { getAuth } from 'firebase/auth'
+import { LogoutIcon } from '@heroicons/vue/outline'
+const props = defineProps(['modelValue', 'user'])
+const emits = defineEmits(['update:modelValue'])
+let style = $computed(() => ({ right: props.modelValue ? 0 : '-18rem' }))
+const auth = getAuth()
+</script>
+
+<template>
+  <transition name="fade">
+    <div v-if="props.modelValue" @click="emits('update:modelValue', false)" class="fixed w-full h-screen bg-black opacity-30 z-50 top-0" />
+  </transition>
+  <div class="all-transition bg-white w-72 h-screen overflow-y-auto top-0 z-50 fixed p-3 sm: p-6" style="max-width: 90vw;" :style="style">
+    <div class="flex items-center" v-if="props.user">
+      <img :src="props.user.photoURL" class="w-10 mr-4 rounded-full">
+      <div>
+        <h3 class="text-xl">{{ props.user.displayName }}</h3>
+        <p class="text-sm text-gray-500">{{ props.user.email }}</p>
+      </div>
+    </div>
+    <h2 class="text-2xl" v-else>Please login</h2>
+    <div v-if="props.user" class="mt-3">
+      <button class="text-gray-500 flex items-center w-full border border-x-0 group" @click="auth.signOut">
+        <logout-icon class="all-transition w-6 m-2 group-hover:mr-3" />Sign out
+      </button>
+    </div>
+  </div>
+</template>
