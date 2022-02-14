2b3213df844572d5e85f511fbe9f928dd4b5c13c
Phantomlsh <phantomlsh@outlook.com>
2022-02-13 17:09:29 -0800

    Complete basic course

---
+++ src/components/Course.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { watch } from 'vue'
+import { getDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.js'
+const props = defineProps(['modelValue', 'quarter'])
+const emits = defineEmits(['update:modelValue'])
+import Wrapper from './Wrapper.vue'
+
+// control UI
+let isMobile = $ref(false)
+window.onresize = () => { isMobile = window.innerWidth < 768 }
+window.onresize()
+
+const levels = { U: 'Undergraduate', G: 'Graduate', L: 'Lower Division', S: 'Upper Division' }
+const gradings = { null: 'optional', L: 'Letter', P: 'P/NP' }
+
+let course = $ref({})
+let title = $computed(() => props.modelValue ? props.modelValue + ': ' + (course.title || '') : 'Please select a course')
+
+watch(() => props.modelValue, async v => {
+  if (!v) return
+  course = {}
+  course = await getDoc(doc(db, 'course', props.quarter + v)).then(r => r.data())
+  setTimeout(() => { course.show = 1 })
+  console.log(course)
+})
+</script>
+
+<template>
+  <transition name="fade">
+    <div v-if="isMobile && props.modelValue" @click="emits('update:modelValue', false)" class="fixed w-full h-screen bg-black opacity-30 z-50 top-0" />
+  </transition>
+  <div class="bg-white p-4 sm:p-6 flex-grow all-transition
+    fixed top-0 h-screen w-5/6 z-50
+    md:sticky md:top-20 md:w-auto md:h-auto md:shadow-md md:z-10
+  " :style="{ left: isMobile && !props.modelValue ? '-100%' : '0px' }">
+    <h2 class="text-xl sm:text-2xl font-bold">{{ title }}</h2>
+    <wrapper :show="course.show" :key="props.modelValue">
+      <p class="text-sm text-gray-600">{{ course.college }} &nbsp; {{ course.department }} &nbsp; {{ levels[course.level] }}</p>
+      <p class="my-2">{{ course.description }}</p>
+      <p><b>Units:</b> {{ course.units }}</p>
+      <p><b>Grading:</b> {{ gradings[course.grading] }}</p>
+      <p><b>Contact Hours:</b> {{ course.hours }}</p>
+      <p v-if="course.GE" class="flex flex-wrap items-center">
+        <b>GE: </b>
+        <label v-for="g in course.GE" class="border text-sm border-blue-400 text-blue-400 bg-blue-100 rounded px-1 m-1">{{ g }}</label>
+        <span v-if="!course.GE.length"> N/A</span>
+      </p>
+    </wrapper>
+  </div>
+</template>
--- src/views/Course.vue
+++ src/views/Course.vue
@@ -4,11 +4,12 @@ import { getDoc, doc } from 'firebase/firestore'
 import { db, log } from '../firebase.js'
 import * as parse from '../utils/parse.js'
 import debounce from '../utils/debounce.js'
+import Course from '../components/Course.vue'
 
 log('course')
 
 let loading = $ref(true), list = $ref(null), showList = $ref({})
-let quarters = $ref([]), quarter = $ref(''), search = $ref('')
+let quarters = $ref([]), quarter = $ref(''), search = $ref(''), focus = $ref('')
 
 const computeResult = debounce(() => {
   showList = {}
@@ -26,6 +27,7 @@ getDoc(doc(db, 'cache', 'quarter')).then(r => {
 
 watch($$(quarter), async v => {
   loading = true
+  focus = false
   list = await getDoc(doc(db, 'cache', 'course.' + v)).then(r => JSON.parse(r.data().data))
   showList = JSON.parse(JSON.stringify(list))
   loading = false
@@ -39,16 +41,19 @@ watch($$(search), computeResult)
   <div class="p-4 sm:p-10">
     <h1 class="text-2xl flex items-center">
       Course
-      <select class="text-base bg-white border mx-2 px-2 py-1 rounded-full" v-if="quarters.length">
+      <select class="text-base bg-white border mx-2 px-2 py-1 rounded-full" v-if="quarters.length" v-model="quarter">
         <option v-for="q in quarters" :value="q">{{ parse.quarter(q) }}</option>
       </select>
     </h1>
     <p v-if="loading" class="text-sm text-gray-500 mb-4">Loading...</p>
-    <input class="my-4 bg-white rounded shadow-md py-1 px-2" type="text" v-model="search" placeholder="Search Course ID or Title">
-    <div>
-      <div class="bg-white shadow rounded p-2 my-2" v-for="(v, k) in showList">
-        <h3 class="font-bold">{{ k }}: {{ v }}</h3>
+    <input class="my-4 bg-white rounded shadow-md py-2 px-3" type="text" v-model="search" placeholder="Search Course ID or Title">
+    <div class="flex items-start" v-if="!loading">
+      <div class="w-full md:w-80 md:mr-6 shadow-md" style="min-width: 20rem;">
+        <div class="bg-white border p-2 cursor-pointer" v-for="(v, k) in showList" @click="focus = k">
+          <h3 class="font-bold">{{ k }}: {{ v }}</h3>
+        </div>
       </div>
+      <course v-model="focus" :quarter="quarter" />
     </div>
   </div>
 </template>
