709cb62698ab985582f9531a69dd6aae502ed2c9
Phantomlsh <phantomlsh@outlook.com>
2022-02-26 12:45:25 -0800

    Update Dining.vue

--- src/views/Dining.vue
+++ src/views/Dining.vue
@@ -29,16 +29,15 @@ getDoc(doc(db, 'cache', 'dining')).then(r => {
       </select>
       <p v-if="!data[dc]">Closed</p>
       <div v-else v-for="(v, mc) in data[dc]" class="rounded shadow-md p-3 my-4" :key="dc">
-        <h3 class="text-xl font-bold">{{ mc }}</h3>
+        <h3 class="text-xl font-bold">{{ mc[0].toUpperCase() + mc.substring(1) }}</h3>
         <p class="text-sm text-gray-500">{{ v ? v.hour : 'closed' }}</p>
         <hr class="mt-1 mb-2">
         <div v-if="v" v-for="(names, station) in v.menu" class="px-2">
           <h4 class="font-bold mt-2 mb-1">{{ station }}</h4>
-          <wrapper :show="1" class="px-2 ml-1 border-l-2 bg-blue-50 border-blue-600">
+          <wrapper :show="1" class="px-2 ml-1 border-l-2 bg-gradient-to-r from-blue-50 to-transparent border-blue-600">
             <div v-for="n in names" class="text-gray-700 hover:pl-1 all-transition">{{ n }}</div>
           </wrapper>
         </div>
-        
       </div>
     </div>
   </div>
