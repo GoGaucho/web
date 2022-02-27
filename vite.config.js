16bc7caaa081fda2b2abb8c715005931a6087f54
Phantomlsh <phantomlsh@outlook.com>
2022-02-27 14:18:14 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: { 'https://us-central1-firebase-gogaucho.cloudfunctions.net/': 'https://us-central1-firebase-gogaucho.cloudfunctions.net/' }
-  },
   plugins: [
     vue({ script: { refSugar: true } }),
     VitePWA({
