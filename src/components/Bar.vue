499b6921aad7611434638e6b0564af2516650ea0
Phantomlsh <phantomlsh@outlook.com>
2022-03-16 21:28:13 -0700

    Upgrade google login

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -2,35 +2,6 @@
 import User from './User.vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
-import { getAuth, signInWithCredential, GoogleAuthProvider } from 'firebase/auth'
-import { log, state } from '../firebase.js'
-
-const provider = new GoogleAuthProvider(), auth = getAuth()
-let gAuth = null
-let userInfo = $ref(null), showUser = $ref(false), ready = $ref(false)
-
-async function listener (user) {
-  const profile = user.getBasicProfile()
-  state.token = user.getAuthResponse().id_token
-  if (!profile || !state.token) return userInfo = showUser = null
-  await signInWithCredential(auth, GoogleAuthProvider.credential(state.token))
-  userInfo = {
-    name: profile.getName(),
-    photoURL: profile.getImageUrl(),
-    email: profile.getEmail()
-  }
-  log('web_login')
-}
-
-gapi.load('auth2', () => {
-  gAuth = gapi.auth2.init({ client_id: '1083649636208-smr7a1d16cl4bl9ufmn0otn8b1pnk4jc.apps.googleusercontent.com', hosted_domain: 'ucsb.edu' })
-  gAuth.then(() => { setTimeout(() => { ready = true }, 500) })
-  gAuth.currentUser.listen(listener)
-})
-
-function login () {
-  gAuth.signIn({ scope: 'profile email' })
-}
 </script>
 
 <template>
@@ -38,10 +9,6 @@ function login () {
     <h1 class="text-2xl font-bold flex items-center cursor-pointer group" @click="router.push('/')">
       <img class="mr-2 w-8 rounded" src="/icons/logo.png">GoGaucho
     </h1>
-    <template v-if="ready">
-      <img v-if="userInfo" class="w-8 rounded-full cursor-pointer" :src="userInfo.photoURL" @click="showUser = true">
-      <button v-else @click="login" class="all-transition rounded px-4 py-1 hover:shadow-md bg-blue-500 text-white">Sign in</button>
-    </template>
+    <user></user>
   </div>
-  <user v-model="showUser" :user="userInfo" />
 </template>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,30 +1,69 @@
 <script setup>
+import { onMounted } from 'vue'
 import { CalendarIcon } from '@heroicons/vue/outline'
 import Wrapper from './Wrapper.vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
-const props = defineProps(['modelValue', 'user'])
-const emits = defineEmits(['update:modelValue'])
 
-function signOut () {
-  gapi.auth2.getAuthInstance().signOut()
-}
+let showPanel = $ref(false), user = $ref({})
 
 function goto (path) {
   router.push(path)
-  emits('update:modelValue', false)
+  showPanel = false
+}
+
+// following are authentication logic
+import { getAuth, signInWithCredential, GoogleAuthProvider } from 'firebase/auth'
+import { log, state } from '../firebase.js'
+
+const provider = new GoogleAuthProvider(), auth = getAuth()
+async function listener (raw) {
+  showPanel = false
+  const token = raw.credential
+  if (!token) return
+  const res = await signInWithCredential(auth, GoogleAuthProvider.credential(token)).catch(() => false)
+  if (!res?.user) return user = {}
+  state.token = token
+  user = {
+    name: res.user.displayName,
+    email: res.user.email,
+    photoURL: res.user.photoURL
+  }
+  log('web_login')
+  showPanel = true
+}
+
+google.accounts.id.initialize({
+  client_id: '1083649636208-smr7a1d16cl4bl9ufmn0otn8b1pnk4jc.apps.googleusercontent.com',
+  hosted_domain: 'ucsb.edu',
+  auto_select: true,
+  cancel_on_tap_outside: false,
+  callback: listener
+})
+google.accounts.id.prompt()
+onMounted(() => {
+  google.accounts.id.renderButton(document.getElementById('signin'), { type: 'icon', size: 'medium', shape: 'circle' })
+})
+
+function signOut () {
+  showPanel = false
+  user = {}
+  state.token = ''
+  auth.signOut()
 }
 </script>
 
 <template>
+  <div id="signin" :class="user.name ? 'hidden' : ''" />
+  <img v-if="user.photoURL" class="w-8 rounded-full cursor-pointer" :src="user.photoURL" @click="showPanel = true">
   <transition name="fade">
-    <div v-if="props.modelValue" @click="emits('update:modelValue', false)" class="fixed w-full h-screen bg-transparant z-50 top-0" />
+    <div v-if="showPanel" @click="showPanel = false" class="fixed w-full h-screen bg-transparant z-50 top-0" />
   </transition>
-  <div class="fixed z-50 top-16 w-72 rounded shadow-lg bg-white all-transition" :class="props.modelValue ? 'right-2' : '-right-72'" v-if="props.user">
-    <wrapper :show="props.modelValue" class="pt-4 flex flex-col items-center">
-      <img :src="props.user.photoURL" class="w-16 rounded-full">
-      <h3 class="text-xl">{{ props.user.name }}</h3>
-      <p class="text-sm text-gray-500">{{ props.user.email }}</p>
+  <div class="fixed z-50 top-16 w-72 rounded shadow-lg bg-white all-transition" :class="showPanel ? 'right-2' : '-right-72'">
+    <wrapper :show="showPanel" class="pt-4 flex flex-col items-center">
+      <img :src="user.photoURL" class="w-16 rounded-full">
+      <h3 class="text-xl">{{ user.name }}</h3>
+      <p class="text-sm text-gray-500">{{ user.email }}</p>
       <button class="all-transition text-gray-500 border rounded px-4 py-1 my-2 hover:bg-gray-50" @click="signOut">Sign out</button>
       <button class="all-transition w-full text-gray-500 flex items-center px-6 py-3 border border-x-0 hover:bg-gray-50" @click="goto('/schedule')">
         <calendar-icon class="w-6 mr-2" />Weekly Schedule
