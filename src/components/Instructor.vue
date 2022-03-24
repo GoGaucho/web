d099a1c6590b5638192afe865bb93801c60de89e
Phantomlsh <phantomlsh@outlook.com>
2022-03-23 19:22:36 -0700

    Update Instructor.vue

--- src/components/Instructor.vue
+++ src/components/Instructor.vue
@@ -14,7 +14,7 @@ watch(() => props.name, async v => {
 
 <template>
   <transition name="fade">
-    <div class="fixed z-50 bottom-4 right-4 w-72 rounded bg-white p-3 ring" v-if="data">
+    <div class="fixed z-50 bottom-4 right-4 w-72 rounded bg-white p-3 ring" v-if="props.name && data">
       <h3 class="font-bold">{{ data.name || props.name }}</h3>
       <p v-if="data.name" class="text-xs text-gray-500">{{ props.name }}</p>
       <p v-if="data.email" class="text-sm select-all"><a :href="'mailto:' + data.email">{{ data.email }}</a></p>
