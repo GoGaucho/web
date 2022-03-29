7b56402f972de042b768caa4e4c9316bf5550c24
Phantomlsh <phantomlsh@outlook.com>
2022-03-28 19:58:09 -0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,8 +35,8 @@ function jump (url) {
       <button @click="router.push('/dining')"><cake-icon class="w-6 mr-2 text-gray-500" />Dining Commons</button>
       <button @click="router.push('/waitz')"><library-icon class="w-6 mr-2 text-gray-500" />Building Capacity</button>
       <button @click="router.push('/course')"><book-open-icon class="w-6 mr-2 text-gray-500" />Course</button>
-      <wrapper :show="state.user.uid">
-        <button @click="router.push('/class')"><calendar-icon class="w-6 mr-2 text-gray-500" />My Classes</button>
+      <wrapper :show="state.user.uid && show" class="p-2">
+        <button @click="router.push('/class')" style="margin: 0px;"><calendar-icon class="w-6 mr-2 text-gray-500" />My Classes</button>
       </wrapper>
     </wrapper>
   </div>
