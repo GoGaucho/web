1c5720ee14b8ad1cbfd027b02f763c65073ce502
Phantomlsh <phantomlsh@outlook.com>
2022-05-09 09:31:42 -0700

    LinkCard (#5)
    * add linkcard
    
    * Fix linkcard style
    
    Co-authored-by: Ari0319 <xinru_li@ucsb.edu>

--- src/views/Link.vue
+++ src/views/Link.vue
@@ -3,6 +3,7 @@ import { log } from '../firebase.js'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 import links from '../utils/links.js'
+import LinkCard from '../components/LinkCard.vue'
 log('web/link')
 </script>
 
@@ -13,16 +14,7 @@ log('web/link')
     <template v-for="c in links">
       <hr>
       <div class="my-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 sm:gap-3">
-        <a v-for="l in c" class="all-transition cursor-pointer rounded flex items-center bg-gradient-to-r from-white to-gray-100 sm:to-white p-2 w-full sm:border hover:shadow hover:from-blue-50" :href="l.href">
-          <picture>
-            <source :srcset="l.icon">
-            <img class="rounded mr-2 object-cover object-top" style="width: 2.5rem; max-height: 2.5rem;" src="/icons/links/UCSB.jpg">
-          </picture>
-          <div class="flex-grow">
-            <h4 class="font-bold">{{ l.name }}</h4>
-            <p class="text-xs text-gray-500">{{ l.description }}</p>
-          </div>
-        </a>
+        <link-card v-for="l in c" :title="l.title" :description="l.description" :icon="l.icon" :href="l.href" />
       </div>
     </template>
   </div>
