43117521b834c1390f588c1da71f981701624b4b
Phantomlsh <phantomlsh@outlook.com>
2022-09-24 15:38:34 -0700

    Update Schedule.vue

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -6,7 +6,17 @@ import state from '../model.js'
 const props = defineProps(['pieces', 'whole'])
 const ds = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-const colors = ['bg-yellow-500', 'bg-purple-500', 'bg-red-500', 'bg-green-500', 'bg-sky-500', 'bg-lime-500', 'bg-orange-500', 'bg-teal-500', 'bg-pink-500']
+const colors = [
+  ['bg-yellow-500', 'text-yellow-700'],
+  ['bg-purple-500', 'text-purple-700'],
+  ['bg-red-500', 'text-red-700'],
+  ['bg-green-500', 'text-green-700'],
+  ['bg-sky-500', 'text-sky-700'],
+  ['bg-lime-500', 'text-lime-700'],
+  ['bg-orange-500', 'text-orange-700'],
+  ['bg-teal-500', 'text-teal-700'],
+  ['bg-pink-500', 'text-pink-700']
+]
 
 let colorMap = $ref({}), labels = $ref({})
 
@@ -23,7 +33,7 @@ watch(() => props.pieces, v => {
   // add new
   let cot = 0
   for (const k of keys) {
-    colorMap[k] = colors[cot++] || 'bg-gray-500'
+    colorMap[k] = colors[cot++] || ['bg-gray-500', 'text-gray-700']
   }
   ls = [...ls].sort()
   for (const l of ls) {
@@ -74,18 +84,18 @@ document.onvisibilitychange = () => { date = new Date() }
         </div>
       </div>
       <div class="flex-grow h-full overflow-y-hidden" :class="props.whole ? 'overflow-x-auto' : 'overflow-x-hidden'"><!-- right -->
-        <div class="grid grid-cols-7 text-center w-full font-bold select-none" :style="{ width: state.screen.md || !props.whole ? '140%' : '100%', height: '24px' }">
+        <div class="grid grid-cols-7 text-center w-full select-none" :style="{ width: state.screen.md || !props.whole ? '140%' : '100%', height: '24px' }">
           <div v-for="d in state.screen.md ? ds : days">{{ d }}</div>
         </div>
         <div class="grid grid-cols-7 gap-px relative w-full overflow-y-hidden" :style="{ width: state.screen.md || !props.whole ? '140%' : '100%', height: '976px' }"><!-- body -->
           <div v-for="j in 112" class="all-transition bg-gray-100 hover:bg-gray-50 rounded" />
           <template v-for="p in props.pieces" :key="p.key">
-            <div v-if="!isHide(p)" :style="pStyle(p)" class="all-transition absolute p-1 text-xs rounded overflow-hidden hover:opacity-30 hover:ring">
-              <div class="font-bold text-shadow text-[0.7rem] sm:text-xs">{{ p.title || p.key }}</div>
-              <div class="text-gray-700 text-[0.6rem] sm:text-xs">{{ p.time }}</div>
-              <div class="text-gray-700 text-[0.625rem] sm:text-xs">{{ p.location }}</div>
-              <div class="all-transition absolute bottom-0 top-0 left-0 w-0.5" :class="colorMap[p.key]" />
-              <div class="absolute bottom-0 top-0 left-0 right-0 opacity-20" :class="colorMap[p.key]" />
+            <div v-if="!isHide(p)" :style="pStyle(p)" class="all-transition absolute p-1 text-xs rounded-r-md overflow-hidden hover:opacity-30">
+              <div class="font-semibold text-shadow text-[0.7rem] sm:text-xs" :class="colorMap[p.key][1]">{{ p.title || p.key }}</div>
+              <div class="text-[0.6rem] sm:text-xs opacity-90" :class="colorMap[p.key][1]">{{ p.time }}</div>
+              <div class="text-[0.625rem] sm:text-xs opacity-90" :class="colorMap[p.key][1]">{{ p.location }}</div>
+              <div class="all-transition absolute bottom-0 top-0 left-0 w-0.5" :class="colorMap[p.key][0]" />
+              <div class="absolute bottom-0 top-0 left-0 right-0" :class="colorMap[p.key][0]" style="opacity: 15%;"/>
             </div>
           </template>
           <div class="flex items-center absolute bg-blue-500 rounded h-0.5" :style="cStyle" />
