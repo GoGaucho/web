e787aac94c29df1f93cb15b5400d04f8f1ca9b21
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-10-22 23:34:27 -0700

    Add map pin icon on hover

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -115,8 +115,9 @@ document.onvisibilitychange = () => { date = new Date() }
         <div class="grid grid-cols-7 gap-px relative w-full overflow-y-hidden all-transition" :style="{ width: state.screen.md || !props.whole ? '140%' : '100%', height: showTime ? '976px' : '736px' }"><!-- body -->
           <div v-for="j in 112" class="all-transition bg-gray-100 hover:bg-gray-50 rounded" />
           <template v-for="p in props.pieces">
-            <div v-if="!isHide(p)" :style="pStyle(p)" class="all-transition absolute p-1 text-xs rounded-r-sm overflow-hidden hover:opacity-30"
-            @click="locate(p.location)" :class="classrooms[p.location] && 'cursor-pointer'">
+            <div v-if="!isHide(p)" :style="pStyle(p)" class="all-transition absolute p-1 text-xs rounded-r-sm overflow-hidden"
+            @click="locate(p.location)" :class="classrooms[p.location] && 'cursor-pointer group'">
+              <MapPinIcon :class="colorMap[p.key][1]" class="w-5 h-5 absolute opacity-0 top-1 right-1 group-hover:opacity-100 transition-opacity" />
               <div class="font-semibold text-shadow text-[0.7rem] sm:text-xs" :class="colorMap[p.key][1]">{{ p.title || p.key }}</div>
               <div class="text-[0.6rem] sm:text-xs opacity-90" :class="colorMap[p.key][1]" v-if="showTime">{{ twelveHour ? twelvify(p.time) : p.time }}</div>
               <div class="text-[0.625rem] sm:text-xs opacity-90" :class="colorMap[p.key][1]">{{ p.location }}</div>
