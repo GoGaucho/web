bb95c7bbfbbb010bd016e58082be3bdb975e4d26
Sammy Lesner <samanthalesner@ucsb.edu>
2025-11-05 19:16:12 -0800

    DONE: add links, fix broken timekeeping link, reorganize links, link to websites/new line for phone numbers, add descriptions & add icons. TODO: turn groups into drop downs with headers?

--- src/utils/links.js
+++ src/utils/links.js
@@ -11,40 +11,83 @@ export default [[{
   title: 'GOLD',
   href: 'https://my.sa.ucsb.edu/gold/',
   description: 'Student Registration Services'
-}],
-[{
-  title: 'Library Room',
-  href: 'https://libcal.library.ucsb.edu/',
-  description: 'Study Room Booking'
 }, {
+  title: 'BARC',
+  href: 'https://mybarc.ucsb.edu/SIWeb/login.jsp',
+  description: 'Billing Information'
+}],
+[ {
   title: 'CLAS',
   href: 'https://myclas.sa.ucsb.edu/login.aspx?ReturnUrl=%2f',
   description: 'Learning Assistance',
   icon: '/icons/links/CLAS.png'
+}, {
+  title: 'Library Room',
+  href: 'https://libcal.library.ucsb.edu/spaces?lid=17859&gid=0',
+  description: 'Study Room Booking'
+} , {
+  title: 'Disabled Students (DSP)',
+  icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5sFLow2UaR_rlscTrUOE1DWolaKL3fIiIWTcsoJhJ7BMevUeGn2jzrcAT5pShvH7CEsk&usqp=CAU',
+  description: 'Learning Accommodations',
+  href: 'https://dsp.sa.ucsb.edu/services/'
+} , {
+  title: 'Associated Students (AS)',
+  description: 'Student Government',
+  href: 'https://www.as.ucsb.edu/',
+  icon: 'https://www.as.ucsb.edu/files/2022/04/logo2022-420px-72x72.png'
 }],
 [{
   title: 'Student Health',
+  description: 'Campus Healthcare Services',
   href: 'https://sso.ucsb.edu/cas/login?service=https%3a%2f%2fmyhealth.ucsb.edu%2f',
   icon: '/icons/links/SH.png'
 }, {
-  title: 'Disabled Students',
-  href: 'https://dsp.sa.ucsb.edu/services/'
+  title: 'Basic Needs',
+  href: 'https://basicneeds.ucsb.edu/',
+  icon: 'https://basicneeds.ucsb.edu/sites/default/files/2023-08/thrivelogowithbacund.gif',
+  description: 'Essential Student Resources'
+}, {
+  title: 'CAPS (24/7)',
+  href: 'https://caps.sa.ucsb.edu/',
+  description: 'Counseling & Psychological Services',
+  phone: '(805) 893-4411',
+    // href: 'tel:8058934411',
+  icon: 'http://www.ucsbmhp.com/uploads/1/0/8/8/108877365/editor/caps-logo-color-fw_1.png'
 }],
 [{
-  title: 'BARC',
-  href: 'https://mybarc.ucsb.edu/SIWeb/login.jsp',
-  description: 'Billing Information'
+  title: 'UC Police',
+  href: 'https://www.police.ucsb.edu/',
+  icon: 'https://www.police.ucsb.edu/sites/default/files/UCPD_Badge-High-ResNoBackground.png',
+  // href: 'tel:8058933446',
+  phone: '(805) 893-3446'
 }, {
+  title: 'CSO Escort',
+  href: 'https://www.police.ucsb.edu/cso/cso-safety-escorts',
+  icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUkO2XdcKHxNQPLOmxSQ0cWiJ9Eap2Epx_Mg&s',
+  description: 'Free Safety Campus Escort',
+  phone: '(805) 893-2000'
+  // href: 'tel:8058932000',
+}, {
+  title: 'ICE Sighting Tracker',
+  description: 'Daily Nexus',
+  href: 'https://dailynexus.com/2025-07-04/daily-nexus-i-c-e-sighting-tracker/',
+  icon: 'https://dailynexus.s3.us-west-1.amazonaws.com/dailynexus/wp-content/uploads/2015/10/cropped-new-logo-192x192.png'
+}],
+[ {
   title: 'Access Card',
   href: 'https://ucsb-sp.transactcampus.com/eaccounts/AccountSummary.aspx?menu=0',
   description: 'GauchoBuck & Meal Plan'
 }, {
-  title: 'Kronos',
-  href: 'https://logon.timekeeping.ucsb.edu/wfc/navigator/logon',
+  title: 'Timekeeping',
+  href: 'https://ucsb-sso.prd.mykronos.com/wfd/home',
   description: 'Electronic Working Timecard',
-  icon: '/icons/links/Kronos.jpg'
-}],
-[{
+  // icon: '/icons/links/Kronos.jpg'
+}, {
+  title: 'ScholarshipUniverse',
+  href: 'https://www.finaid.ucsb.edu/scholarships/scholarship-universe',
+  description: 'UCSB Scholarship Tool'
+} ], 
+[ {
   title: 'Gaucho Print',
   href: 'https://gauchoprint.ucsb.edu/user',
   icon: 'https://gauchoprint.ucsb.edu/images/icons3/favicon.ico'
@@ -53,21 +96,13 @@ export default [[{
   href: 'https://sso.services.box.net/sp/startSSO.ping?PartnerIdpId=urn:mace:incommon:ucsb.edu&TargetResource=https%3A%2F%2Fucsb.account.box.com%2Fsso%2Fping_federate%3Ffrom%3Dbox%26redirect_url%3D%252F',
   icon: '/icons/links/Box.png',
   description: 'Box Cloud Storage'
-}, {
-  title: 'AS',
-  href: 'https://www.as.ucsb.edu/',
-  icon: 'https://www.as.ucsb.edu/files/2022/04/logo2022-420px-72x72.png'
 }],
 [{
-  title: 'UC Police',
-  href: 'tel:8058933446',
-  description: '(805) 893-3446'
+  title: 'UCSB Platinum',
+  href: 'https://ucsbplat.com/',
+  icon: 'https://ucsbplat.com/static/gaucho.png'
 }, {
-  title: 'CSO Escort',
-  href: 'tel:8058932000',
-  description: '(805) 893-2000'
-}, {
-  title: 'CAPS (24/7)',
-  href: 'tel:8058934411',
-  description: '(805) 893-4411'
+  title: 'StorkeCentral',
+  href: 'https://apps.apple.com/pl/app/storkecentral/id1594777645',
+  icon: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/e8/9f/22/e89f22bf-4dae-3373-f927-4121d971a97d/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/230x0w.webp'
 }]]
