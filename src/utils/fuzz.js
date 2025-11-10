15e317446070127fcf11bb80729dc5c979bbd3ca
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-11-10 13:00:30 -0800

    Create fuse config file

---
+++ src/utils/fuzz.js
@@ -0,0 +1,11 @@
+export const courseFuseOptions = {
+
+    includeMatches: false,
+    threshold: 0.4,
+    keys: [
+        "key",
+        "name"
+    ]
+};
+
+// in case fuzzy needed elsewhere, put separate configs here
