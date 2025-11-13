1dd6948ff9fb1bb0d6cc25e4373d78cf8b4addce
Jayden Tan <jaydentan@ucsb.edu>
2025-11-13 11:57:35 -0800

    Fixed redirect to only occur on mapIcon click

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -115,14 +115,14 @@ document.onvisibilitychange = () => { date = new Date() }
         <div class="grid grid-cols-7 gap-px relative w-full overflow-y-hidden all-transition" :style="{ width: state.screen.md || !props.whole ? '140%' : '100%', height: showTime ? '976px' : '736px' }"><!-- body -->
           <div v-for="j in 112" class="all-transition bg-gray-100 hover:bg-gray-50 rounded" />
           <template v-for="p in props.pieces">
-            <div v-if="!isHide(p)" :style="pStyle(p)" class="all-transition absolute p-1 text-xs rounded-r-sm overflow-hidden"
-            @click="locate(p.location)" :class="classrooms[p.location] && 'cursor-pointer group'">
-              <MapPinIcon :class="colorMap[p.key][1]" class="w-5 h-5 absolute opacity-0 top-1 right-1 group-hover:opacity-100 transition-opacity" />
+            <div v-if="!isHide(p)" :style="pStyle(p)" class="all-transition absolute p-1 text-xs rounded-r-sm overflow-hidden" :class="classrooms[p.location] ? 'group' : ''"
+            >
+              <MapPinIcon v-if="classrooms[p.location]" @click.stop="locate(p.location)" :class="colorMap[p.key][1]" class="w-5 h-5 absolute opacity-0 top-1 right-1 group-hover:opacity-100 transition-opacity cursor-pointer"/>
               <div class="font-semibold text-shadow text-[0.7rem] sm:text-xs" :class="colorMap[p.key][1]">{{ p.title || p.key }}</div>
               <div class="text-[0.6rem] sm:text-xs opacity-90" :class="colorMap[p.key][1]" v-if="showTime">{{ twelveHour ? twelvify(p.time) : p.time }}</div>
               <div class="text-[0.625rem] sm:text-xs opacity-90" :class="colorMap[p.key][1]">{{ p.location }}</div>
               <div class="all-transition absolute bottom-0 top-0 left-0 w-0.5" :class="colorMap[p.key][0]" />
-              <div class="absolute bottom-0 top-0 left-0 right-0" :class="colorMap[p.key][0]" style="opacity: 15%;"/>
+              <div class="absolute bottom-0 top-0 left-0 right-0 pointer-events-none" :class="colorMap[p.key][0]" style="opacity: 15%;"/>
             </div>
           </template>
           <div class="flex items-center absolute" :style="cStyle">
