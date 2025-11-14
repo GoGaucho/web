9975b2ba314225c494ebc74ee09ac5176e4df9e8
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-11-13 23:52:25 -0800

    Create fuzzy and course data flatten helpers

--- src/utils/fuzz.js
+++ src/utils/fuzz.js
@@ -3,9 +3,38 @@ export const courseFuseOptions = {
     includeMatches: false,
     threshold: 0.4,
     keys: [
-        "key",
-        "name"
+        "id",
+        "name",
     ]
 };
+// in case fuzzy needed elsewhere, put separate configs below
 
-// in case fuzzy needed elsewhere, put separate configs here
+
+export function fuzzyQuery(query, fuseObj, list) {
+    if (!query) return list
+    if (!fuseObj) return list
+
+    const normalized = query.replaceAll(' ', '').toUpperCase()
+
+    // Substring search first
+    const substringMatches = list.filter(item =>
+        item.id?.includes(normalized) || item.name?.toUpperCase().includes(normalized)
+    )
+
+    if (substringMatches.length > 0) return substringMatches
+
+    // Corrections after using fuzzy match
+    return fuseObj.search(query).map(r => r.item)
+}
+
+// Flattens course data specifically for fuzzy searchability
+export function flattenCourseData(unflattenedCourses) {
+    const flat = [];
+    for (const subject in unflattenedCourses) {
+        for (const course in unflattenedCourses[subject]) {
+            const courseInfo = unflattenedCourses[subject][course];
+            flat.push({ subject, id: course, name: courseInfo[0] })
+        }
+    }
+    return flat;
+}
